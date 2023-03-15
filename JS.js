83db83e02a3cad21b87e40167a7a1aa31cba5b8c
RodrigoMMont <rodrigo.ommonteiro@gmail.com>
2023-03-15 11:48:39 -0300

    Initial commit

---
+++ JS.js
@@ -0,0 +1,15 @@
+
+window.onscroll = function() {
+    rolagem()
+}
+
+function rolagem() {
+    if(document.body.scrollTop > 760 || document.documentElement.scrollTop > 760) {
+        document.getElementsByTagName("header")[0].style.backgroundColor = "black";
+        document.getElementsByTagName("header")[0].style.transition = "1s";
+    }
+    else {
+        document.getElementsByTagName("header")[0].style.backgroundColor = "rgb(10, 10, 10, 0)";
+        document.getElementsByTagName("header")[0].style.transition = "1s";
+    }
+}
